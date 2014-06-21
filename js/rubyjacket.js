e62d1c358ef100b354da8c07476225222a2f95b0
Rob Jauquet <rjauquet@gmail.com>
2014-06-21 14:37:11 -0500

    adding navigation slider

--- js/rubyjacket.js
+++ js/rubyjacket.js
@@ -4,3 +4,9 @@ $(function() {
         layoutMode: 'fitRows'
     });
 });
+
+$("#toggle-shown").click(function (){
+    $("#navigation").toggle();
+    $("#toggle-shown").toggleClass("toggle-show");
+    $("#toggle-shown").toggleClass("toggle-hide");
+});
