778180c94babd2fb6345abc62018a1cc76532579
Rob Ervin Jauquet <rob.jauquet@zapier.com>
2018-05-20 13:57:55 -0500

    adding blog model

--- examples/search/models.py
+++ examples/search/models.py
@@ -1,3 +1,17 @@
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    title = models.TextField()
+    text = models.TextField()
+
+    title_vector = SearchVectorField(null=True)
+    text_vector = SearchVectorField(null=True)
+
+    class Meta:
+        indexes = [GinIndex(fields=[
+            'text_vector',
+            'title_vector',
+        ])]
